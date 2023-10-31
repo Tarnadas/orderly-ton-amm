eeaa005e65b3e12a20d7bfd2733947f2549b80c1
Mario Reder <mario.reder@pm.me>
2023-10-31 22:10:06 +0100

    deploy scripts

--- scripts/deployJettonMaster.ts
+++ scripts/deployJettonMaster.ts
@@ -1,9 +1,18 @@
-import { toNano } from 'ton-core';
-import { JettonMaster } from '../wrappers/JettonMaster';
+import { config } from 'dotenv';
+import { Address, Builder, Cell, toNano } from 'ton-core';
+import { JettonMaster, Mint } from '../wrappers/JettonMaster';
 import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const jettonMaster = provider.open(await JettonMaster.fromInit());
+    config();
+    if (!process.env.OWNER_ADDRESS) {
+        throw new Error('`OWNER_ADDRESS` environment variable required');
+    }
+    if (!process.env.MNEMONIC) {
+        throw new Error('`MNEMONIC` environment variable required');
+    }
+    const owner = Address.parse(process.env.OWNER_ADDRESS);
+    const jettonMaster = provider.open(await JettonMaster.fromInit(owner, Cell.EMPTY));
 
     await jettonMaster.send(
         provider.sender(),
@@ -18,5 +27,19 @@ export async function run(provider: NetworkProvider) {
 
     await provider.waitForDeploy(jettonMaster.address);
 
-    // run methods on `jettonMaster`
+    // const receiver = Address.parseRaw('0:5845c15abe7af4a1aad752de181ce89304f41d6fa58a44e40d200720b6e401a0');
+    // await jettonMaster.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano('0.1'),
+    //     },
+    //     {
+    //         $$type: 'Mint',
+    //         amount: toNano('100'),
+    //         receiver,
+    //         responseDestination: receiver,
+    //     }
+    // );
+
+    // EQAOj2JC6zZlMr4-gqKEChqeUNBQMkn_IBIgQQNFglM6dELe
 }
--- scripts/deployOrderlyAmm.ts
+++ scripts/deployOrderlyAmm.ts
@@ -1,9 +1,18 @@
-import { toNano } from 'ton-core';
+import { config } from 'dotenv';
+import { Address, toNano } from 'ton-core';
 import { OrderlyAmm } from '../wrappers/OrderlyAmm';
 import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const orderlyAmm = provider.open(await OrderlyAmm.fromInit(BigInt(Math.floor(Math.random() * 10000))));
+    config();
+    if (!process.env.OWNER_ADDRESS) {
+        throw new Error('`OWNER_ADDRESS` environment variable required');
+    }
+    if (!process.env.MNEMONIC) {
+        throw new Error('`MNEMONIC` environment variable required');
+    }
+    const owner = Address.parse(process.env.OWNER_ADDRESS);
+    const orderlyAmm = provider.open(await OrderlyAmm.fromInit(owner));
 
     await orderlyAmm.send(
         provider.sender(),
@@ -18,5 +27,6 @@ export async function run(provider: NetworkProvider) {
 
     await provider.waitForDeploy(orderlyAmm.address);
 
-    console.log('ID', await orderlyAmm.getId());
+    console.log(`Deployed AMM: ${orderlyAmm.address}`);
+    // EQBtn3U97Mb4zqlK61PgxADpvOygiiLmmKwW_l9J3t6c3QHp
 }
